4477cc0a28b00edd571d5af5630e11b5b93faa11
Tom Ross <tom@umpox.com>
2016-05-05 20:46:14 +0100

    Add logic to determine reading time

--- popup.js
+++ popup.js
@@ -1,3 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-
+    var wordCount = document.body.innerText.split(' ').length;
+    var readingSpeed = 275; //Words per minute
+    var timeToRead = wordCount / readingSpeed;
+    console.log('This will take' + timeToRead + 'to read');
 });
