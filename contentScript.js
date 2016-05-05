ca4bfb86c543bb1181e6cfe11ead2236ac8833a2
Tom Ross <tom@umpox.com>
2016-05-05 23:11:41 +0100

    Calculate timeToRead to 2 decmial places

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,4 @@
 var wordCount = document.body.innerText.split(' ').length;
 var readingSpeed = 275; //Words per minute
-var timeToRead = wordCount / readingSpeed;
+var timeToRead = (wordCount / readingSpeed).toFixed(2);
 console.log('This will take' + timeToRead + 'to read!!!');
