5252f9ea54dc91bd827c84b6d64ac78a66f46b3d
Tom Ross <tom@umpox.com>
2016-05-05 22:12:08 +0100

    Changed event page to content script that will run on page load

---
+++ contentScript.js
@@ -0,0 +1,4 @@
+var wordCount = document.body.innerText.split(' ').length;
+var readingSpeed = 275; //Words per minute
+var timeToRead = wordCount / readingSpeed;
+console.log('This will take' + timeToRead + 'to read');
