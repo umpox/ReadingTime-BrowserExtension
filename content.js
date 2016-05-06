75486492233f1746cd4a29ab95e9efa4c4477321
Tom Ross <tom@umpox.com>
2016-05-06 16:48:56 +0100

    Remove unneeded console.log statement

--- content.js
+++ content.js
@@ -1,7 +1,6 @@
 var wordCount = document.body.innerText.split(' ').length;
 var readingSpeed = 275; //Words per minute
 var timeToRead = (wordCount / readingSpeed).toFixed(2);
-console.log('This will take' + timeToRead + 'to read!!!');
 
 chrome.runtime.sendMessage({newBadgeText: timeToRead}, 
   function(response){
