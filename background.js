64d92c77773c4d06b5c070bae4fc3cd5b1038c3a
Tom Ross <tom@umpox.com>
2016-05-06 23:01:50 +0100

    Change icon via canvas every time a new reading time is generated

--- background.js
+++ background.js
@@ -1,5 +1,22 @@
+var canvas = document.createElement('canvas');
+var context = canvas.getContext('2d');
+var newBadge;
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request) {
-    chrome.browserAction.setBadgeText({text: request.newBadgeText + " min"});
+    canvas.width = 19;
+    canvas.height = 19;
+    
+    context.textAlign = "center";
+    context.textBaseline = "middle";
+    context.font = "18px Arial";
+    context.fillText(request.newBadge, 8, 8);
+    context.fillStyle = "#FFFFFF";
+    
+    newBadge = context.getImageData(0, 0, 19, 19);
+    
+    chrome.browserAction.setIcon({
+      imageData: newBadge
+    });
   }
 });
--- content.js
+++ content.js
@@ -2,7 +2,7 @@ var wordCount = document.body.innerText.split(' ').length;
 var readingSpeed = 275; //Words per minute
 var timeToRead = Math.ceil(wordCount / readingSpeed);
 
-chrome.runtime.sendMessage({newBadgeText: timeToRead}, 
+chrome.runtime.sendMessage({newBadge: timeToRead}, 
   function(response){
       //Respond to badge successfully changing?
   }
