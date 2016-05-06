fbe6014b6e76f896bb4a1440defc8c1635784acc
Tom Ross <tom@umpox.com>
2016-05-06 16:46:46 +0100

    Send timeToRead variable from content script to background.js to make use of Chrome APIs

--- content.js
+++ content.js
@@ -2,3 +2,9 @@ var wordCount = document.body.innerText.split(' ').length;
 var readingSpeed = 275; //Words per minute
 var timeToRead = (wordCount / readingSpeed).toFixed(2);
 console.log('This will take' + timeToRead + 'to read!!!');
+
+chrome.runtime.sendMessage({newBadgeText: timeToRead}, 
+  function(response){
+      //Respond to badge successfully changing?
+  }
+);
