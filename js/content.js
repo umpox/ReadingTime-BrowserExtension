416f0cf38413332cb77e6cb4f6bb896c2e390a80
Tom Ross <tom@umpox.com>
2016-05-14 21:47:03 +0100

    Update content script to use chrome storage api

--- js/content.js
+++ js/content.js
@@ -1,11 +1,36 @@
 var wordCount = document.body.innerText.split(' ').length;
-var readingSpeed = 275; //Words per minute
-var timeToRead = Math.ceil(wordCount / readingSpeed);
-var iconColor = "#000000";
-var message = {
-  timeToRead: timeToRead,
-  iconColor: iconColor
-};
+var readingSpeed;
+var timeToRead;
+
+//var readingSpeed = 275; //Words per minute
+//var timeToRead = Math.ceil(wordCount / readingSpeed);
+var iconColor;
+var message = {};
+
+chrome.storage.local.get(null, function(result){
+  if (result.rSpeed === undefined) {
+    readingSpeed = 275;
+  }
+  else {
+    readingSpeed = result.rSpeed;
+  }
+  
+  if (result.iColor === undefined) {
+    iconColor = "#000000";
+  }
+  else {
+    iconColor = result.iColor;
+  }
+  
+});
+
+
+  message.iconColor = iconColor;
+  message.timeToRead = Math.ceil(wordCount / readingSpeed);
+  /*chrome.storage.sync.set({'value': theValue}, function() {
+    // Notify that we saved.
+    message('Settings saved');
+  });*/
 
 chrome.runtime.sendMessage({newBadge: message}, 
   function(response){
