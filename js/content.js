824c667a222fcd74ac8a4316f34126199e9fce47
Tom Ross <tom@umpox.com>
2016-05-15 00:02:41 +0100

    Migrate localStorage saving functionality to Chrome storage API

--- js/content.js
+++ js/content.js
@@ -5,19 +5,16 @@ var timeToRead;
 var message = {};
 
 //Check localStorage for saved values
-if (localStorage.readingSpeed !== undefined) {
-  readingSpeed = localStorage.timeToRead;
-}
-if (localStorage.iconColor !== undefined) {
-  iconColor = localStorage.iconColor;
-}
-
-timeToRead = Math.ceil(wordCount / readingSpeed);
-message.timeToRead = timeToRead;
-message.iconColor = iconColor;
-
-chrome.runtime.sendMessage({newBadge: message});
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-
+chrome.storage.local.get(null, function (result) {    
+    if (result.readingSpeed !== undefined) {
+      readingSpeed = result.readingSpeed;
+    }
+    if (result.iconColor !== undefined) {
+      iconColor = result.iconColor;
+    }
+    
+    timeToRead = Math.ceil(wordCount / readingSpeed);
+    message.timeToRead = timeToRead;
+    message.iconColor = iconColor;    
+    chrome.runtime.sendMessage({newBadge: message});
 });
