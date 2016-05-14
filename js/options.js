0f945c6ede53b1adab8537b9da1c684aaedc067f
Tom Ross <tom@umpox.com>
2016-05-15 00:03:32 +0100

    Add options page logic to store user settings

--- js/options.js
+++ js/options.js
@@ -1,33 +1,61 @@
 var saveBtn = document.getElementById('userSettings-list_save');
+var resetBtn = document.getElementById('userSettings-list_reset');
 var wpmValue = document.getElementById('userSettings-list_value_wpm');
 var colorValue = document.getElementById('userSettings-list_value_color');
-var errorMsg = document.getElementById('userSettings-list_error');
+var notifyMsg = document.getElementById('userSettings-list_notify');
+var defaultWpm = 275, defaultColor = "#000000";
 
-//Adding event listener as Chrome does not allow HTML onclick events
+//Adding event listener to buttons as Chrome does not allow HTML onclick events
 saveBtn.addEventListener('click', function() {
   validateInput(wpmValue.value, colorValue.value); 
+});
+
+resetBtn.addEventListener('click', function() {
+  wpmValue.value = defaultWpm;
+  colorValue.value = defaultColor;
+  
+  chrome.storage.local.set({'readingSpeed': defaultWpm});
+  chrome.storage.local.set({'iconColor': defaultColor});
+  
+  notifyMsg.innerHTML = "Success! Your values have been reset to default and saved.";
+  notifyMsg.style.color = "green";
+});
+
+//Check storage for current color and reading speed values
+chrome.storage.local.get(null, function (result) {    
+  if (result.readingSpeed !== undefined) {
+    wpmValue.value = result.readingSpeed;
+  }
+  else {
+    wpmValue.value = defaultWpm;
+  }
+  if (result.iconColor !== undefined) {
+    colorValue.value = result.iconColor;
+  }
+  else {
+    colorValue.value = defaultColor;
+  }
 });
 
 function validateInput(wpm, color) {
   var colorRegex = /^#([0-9a-f]{6}|[0-9a-f]{3})$/i;
   var numRegex = /^\d+$/;
-  var userInputs = {};
   
   if (wpm === '' || color === '') {
-    errorMsg.innerHTML = 'Error: Please ensure all fields have a value.';
+    notifyMsg.innerHTML = 'Error: Please ensure all fields have a value.';
+    notifyMsg.style.color = "green";
     return;
   }
   
   if ( numRegex.test(wpm) && colorRegex.test(color) ) {
-    userInputs.wpm = wpm;
-    userInputs.color = color;
-    
-    chrome.runtime.sendMessage({userInputs: userInputs}, 
-      function(response){}
-    );
+    chrome.storage.local.set({'readingSpeed': wpm});
+    chrome.storage.local.set({'iconColor': color});
+    notifyMsg.innerHTML = "Success! Your new values have been saved.";
+    notifyMsg.style.color = "green";
   }
   else {
-    errorMsg.innerHTML = 'Error: Incorrect format, please check your values.';
+    notifyMsg.innerHTML = 'Error: Incorrect format, please check your values.';
+    notifyMsg.style.color = "red";
   }
   
 }
